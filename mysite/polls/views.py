d0b3c359b56588f5e41191d5211aa2abf9c4245e
htet_lin_maung <htet.lin.maung.@fusionsol.com>
2020-02-24 15:57:20 +0630

    upload project

---
+++ mysite/polls/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def show(request):
+    return HttpResponse("showing single poll")
+
